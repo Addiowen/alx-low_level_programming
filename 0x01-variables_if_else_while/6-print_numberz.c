aa2339822ce9ac91411be305145a96e519e27b56
Addiowen <owenoscar18@gmail.com>
2022-12-08 21:15:30 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints base ten digits without char
- *
+ * main - prints all single digit numbers of base 10 starting from 0,
+ *followed by a new line, using putchar
  * Return: zero
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 0; i <= 9; i++)
-		putchar(i);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
 	putchar('\n');
 	return (0);
 }

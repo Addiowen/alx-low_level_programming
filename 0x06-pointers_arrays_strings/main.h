07cfc7ba0d3241b6970d31fc6dce82d99bc756fb
Addiowen <owenoscar18@gmai.com>
2022-12-22 03:30:49 -0800

    string_toupper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int string_length(char *pointer);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *);
 
 #endif

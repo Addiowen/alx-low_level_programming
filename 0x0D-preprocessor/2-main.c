3a9b3d10414fc42641ec09d9a95fe30fefcde630
Addiowen <owenoscar18@gmail.com>
2023-01-17 01:14:26 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line.
+ * @argc: returns counter to program executed commands.
+ * @argv: store executed commands as strings.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
